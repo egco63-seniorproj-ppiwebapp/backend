968bb92ecba98251f03c677a3df550b781c09e78
_CY_ <77828453+chavakan209@users.noreply.github.com>
2023-09-22 20:13:49 +0700

    Add files via upload

--- foot_web/urls.py
+++ foot_web/urls.py
@@ -5,14 +5,15 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('get_collection/',csrf_exempt(get_collection),name="get_collection"),
-    path("get_collection/<int:id>", csrf_exempt(get_collection_by_id), name = "get_collection_by_id"),
-    path('add_collection',csrf_exempt(add_collection),name="add_collection"),
-    path('auth',csrf_exempt(auth),name="auth"),
-    path('logout',csrf_exempt(logout_api),name="logout"),
-    path('patch_collection',csrf_exempt(patch_collection),name="patch_collection"),
+    path('api/get_collection/',csrf_exempt(get_collection),name="get_collection"),
+    path("api/get_collection/<int:id>", csrf_exempt(get_collection_by_id), name = "get_collection_by_id"),
+    path('api/add_collection',csrf_exempt(add_collection),name="add_collection"),
+    path('api/auth',csrf_exempt(auth),name="auth"),
+    path('api/logout',csrf_exempt(logout_api),name="logout"),
+    path('api/patch_collection',csrf_exempt(patch_collection),name="patch_collection"),
 
-    path('upload_file/',csrf_exempt(upload_file),name="upload_file"),
-    path("get_img/<int:id>", csrf_exempt(get_img), name = "get_img"),
+    # path('upload_file/',csrf_exempt(upload_file),name="upload_file"),
+    path("api/get_img/<int:id>", csrf_exempt(get_img), name = "get_img"),
+    path('', )
 
 ]
--- footapp/views.py
+++ footapp/views.py
@@ -23,55 +23,20 @@ import base64
 _path = Path(__file__, '..').resolve()
 # Create your views here.
 
-"""
-Diagram
-Login user : same time
-Time line: when should they finish labelling for us
-Hosting : which platform
 
-DINO : Finetune with freeze itself and add extra layers
-
-Croos entrophy: classification
-Triplet loss: k-nn Vectorspace
-
-Maybe create anothor collection_log
 """
 
-USER = {
-    'admin' : 'admin123'
-}
+INPUT/ OUTPUT API
+รับข้อมูลเป็นอะไร
+ข้อมูลออกมาเป็นยังไง
 
-'''
-TODO:
+ชื่อรูปภาพตาม id - Finished
 
-    IMPORTANT: JSONWebToken(cookie) for user in every request 
-        if no cookie found --> send 401 (unauthorize)
+TODO: push get_img(ส่งเป็น contenttypr img): Finised
+multiple unlabel image: Finished
+then call patch for the image if the 
+"""
 
-    1) API --> GET Handle start/end index and filter (use parameter) --> add new GET API [get_collection]
-        1.1) Filter:
-            - label (stat)
-            - side
-        1.2) Search 
-            - name
-        1.3) Sort [by what & ascending\descending]
-            - create at
-            - id
-            - name
-
-    2) API --> PATCH (F_name: patch_collection)
-        body can get one or more of the following:
-            - stat
-            - size
-            - remark
-            - deleted
-
-    3) API --> POST (F_name: auth)
-        body:
-            - username
-            - password
-'''
-
-# NOY FINISED
 @login_required
 def get_collection(request): #1)
     SORT_OPTION = ['created_date', 'pk', 'name']
@@ -133,18 +98,6 @@ def get_collection_by_id(request, id):
     data = serializers.serialize('json', instance)
     return HttpResponse(data, content_type='application/json')
 
-@login_required
-def add_collection(request):
-    if request.method=='POST':
-        received_json_data= json.loads(request.body)
-        try:
-            Database.objects.create(**received_json_data)
-            # Database.save()
-            return HttpResponse('POST successfully!!')
-        except Exception:
-            return HttpResponse('POST successful BUT DID NOT PUT IN DATABASE!!')
-
-    return HttpResponse('We got request')
 
 @login_required
 def patch_collection(request):#2)
@@ -186,15 +139,31 @@ def logout_api(request):
     return HttpResponse('Logout successfully!')
 
 
+
+# @login_required
+# def add_collection(request):
+#     if request.method=='POST':
+#         received_json_data= json.loads(request.body)
+#         try:
+#             Database.objects.create(**received_json_data)
+#             # Database.save()
+#             return HttpResponse('POST successfully!!')
+#         except Exception:
+#             return HttpResponse('POST successful BUT DID NOT PUT IN DATABASE!!')
+
+#     return HttpResponse('We got request')
+
 @login_required
-def upload_file(request):
+def add_collection(request):
     received_json_data= json.loads(request.body)
     gauth = GoogleAuth()
     scope = ["https://www.googleapis.com/auth/drive"]
     _path = os.path.dirname(__file__)
     gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name(_path+'\\credential.json', scope)
     drive = GoogleDrive(gauth)
-    file_name = received_json_data['file_name']+'.jpg'
+    instance = Database.objects.create()
+    data = instance.__dict__
+    file_name = 'img_'+data['name']+'.jpg'
     _file = drive.CreateFile({'parents':[{'id':'1IYdmr-oWqKKCjq-RsjBbS9kEgcnY_G4R'}],'title':f'{file_name}', 'mimeType':'image/jpeg'})
     img_data = received_json_data['img_file']
     with open(_path+'\\temp.jpg', "wb") as fh:
@@ -202,8 +171,8 @@ def upload_file(request):
         fh.close()
     _file.SetContentFile(_path+'\\temp.jpg')
     _file.Upload()
-    Database.objects.filter(name = received_json_data['file_name']).update(link = str(_file['id']))
-    return HttpResponse('Uploaded to GDrive!')
+    Database.objects.filter(name = data['name']).update(link = str(_file['id']))
+    return HttpResponse(data['id'])
 
 @login_required
 def get_img(request, id:int):
@@ -217,5 +186,5 @@ def get_img(request, id:int):
     _file = drive.CreateFile({'id':f'{file_id}', 'mimeType':'image/jpeg'})
     _file.GetContentFile(filename=_path+'\\temp.jpg', mimetype='image/jpeg')
     with open(_path+'\\temp.jpg', "rb") as f:
-        encoded_image = base64.b64encode(f.read())
-        return HttpResponse(encoded_image)
+        img = f.read()
+        return HttpResponse(img, content_type='image/jpeg')
